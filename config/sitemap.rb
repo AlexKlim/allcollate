4a6a398388a99c5ef7f9f7a81cc45bee63e4dac1
Alex <alex.klimenkov@gmail.com>
2020-06-12 01:56:16 +0300

    upload sitemap

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -6,7 +6,12 @@ SitemapGenerator::Sitemap.default_host = 'http://www.allcollate.com'
 SitemapGenerator::Sitemap.sitemaps_host = "https://#{Rails.application.credentials.aws[:fog_upload_bucket]}.s3.amazonaws.com/"
 SitemapGenerator::Sitemap.public_path += 'tmp/' unless File.split(SitemapGenerator::Sitemap.public_path).include?('tmp')
 SitemapGenerator::Sitemap.sitemaps_path = 'sitemaps/'
-SitemapGenerator::Sitemap.adapter = SitemapGenerator::WaveAdapter.new
+
+SitemapGenerator::Sitemap.adapter = SitemapGenerator::S3Adapter.new(fog_provider: 'AWS',
+                                     aws_access_key_id: Rails.application.credentials.aws[:uploads_access_key_id],
+                                     aws_secret_access_key: Rails.application.credentials.aws[:uploads_secret_access_key],
+                                     fog_directory: Rails.application.credentials.aws[:fog_upload_bucket],
+                                     fog_region: 'us-east-1')
 
 SitemapGenerator::Sitemap.compress = false
 
