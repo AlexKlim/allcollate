11f9ea39b5a7c5be212b04849fe0c3573a0e1798
Alex <alex.klimenkov@gmail.com>
2020-06-17 00:04:45 +0300

    add 404 redirect, fix broken pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
+
+  def not_found
+    redirect_to('/', status: 301) if (!request.xhr? && request.get? && request.format == :html)
+  end
 end
