7b9f3ea4e5ff5f3619a61c2e9c1b15f0b573f845
Alex <alex.klimenkov@gmail.com>
2020-10-07 02:18:48 +0300

    update footer add hotel directory

---
+++ app/controllers/frontend/directory/hotels_controller.rb
@@ -0,0 +1,15 @@
+class Frontend::Directory::HotelsController < ApplicationController
+  layout 'frontend'
+
+  # before_action :check_prefix
+
+  PER_PAGE = 75
+
+  def show
+    @hotels = if params[:prefix] == 'more'
+                Hotel.active.ransack(name_not_start_all: ('a'..'z'))
+              else
+                Hotel.active.ransack(name_start: params[:prefix])
+              end.result.page(params[:page]).per(PER_PAGE).order(:name)
+  end
+end
--- app/controllers/frontend/hotels_controller.rb
+++ app/controllers/frontend/hotels_controller.rb
@@ -1,11 +1,7 @@
 class Frontend::HotelsController < ApplicationController
+  layout 'frontend'
 
   def show
     @hotel = Hotel.active.includes(:photos, :rates).friendly.find(params[:id])
-      # .
-      #              to_json(include: {
-      #                photos: { only: [:url, :order] },
-      #                rates: { only: [:currency, :daily_rate, :actual_on, :roomtype_name] }
-      #               })
   end
 end
