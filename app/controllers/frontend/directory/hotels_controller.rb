8b5246e3a967aa9416af19827ca84bd123ba2cea
Alex <alex.klimenkov@gmail.com>
2020-10-07 21:36:58 +0300

    remove order due to performance issue

--- app/controllers/frontend/directory/hotels_controller.rb
+++ app/controllers/frontend/directory/hotels_controller.rb
@@ -10,6 +10,6 @@ class Frontend::Directory::HotelsController < ApplicationController
                 Hotel.active.ransack(name_not_start_all: ('a'..'z'))
               else
                 Hotel.active.ransack(name_start: params[:prefix])
-              end.result.page(params[:page]).per(PER_PAGE).order(:name)
+              end.result.page(params[:page]).per(PER_PAGE)
   end
 end
