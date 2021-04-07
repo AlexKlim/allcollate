2e31abd6b34657eb426020ab6301f99e979a5070
Bilal Ashraf <bilal.ashraf@appsgenii.eu>
2021-04-08 01:06:19 +0500

    changes

--- app/javascript/components/Search/Filters/YearRenovated.js
+++ app/javascript/components/Search/Filters/YearRenovated.js
@@ -15,43 +15,9 @@ import Slider from '@material-ui/core/Slider';
 
 function YearRenovated({ value = '' }) {
   const {
-    query,
-    locations,
-    setActivePage,
-    setLocations,
-    setHotels,
     yearRenovationSlider,
     setYearRenovationSlider,
   } = useSearchContext();
-  const [results, setResults] = useState([]);
-
-  const doSearch = async (query) => {
-    const searchAPI = new SearchAPI();
-    const data = await searchAPI.fetchLocations(query);
-    setResults(data);
-  };
-
-  const doRemoveClick = (event) => {
-    let updatedLocations = [];
-
-    locations.forEach((singleItem) => {
-      if (
-        singleItem.city === event.city &&
-        singleItem.country === event.country
-      ) {
-        // Do not push in array
-      } else {
-        updatedLocations.push(singleItem);
-      }
-    });
-    setActivePage(1);
-    setLocations(updatedLocations);
-  };
-
-  const doSuggestionSelected = async (item) => {
-    setActivePage(1);
-    setLocations((locations) => [...locations, item]);
-  };
 
   const handleChange = (event, newValue) => {
     setYearRenovationSlider(newValue);
