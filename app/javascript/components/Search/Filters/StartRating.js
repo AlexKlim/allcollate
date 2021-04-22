017cfc03ed9da121cba89b39b0dc2f2f7d0bdacb
Bilal Ashraf <bilalashraf141@yahoo.com>
2021-04-22 09:54:27 +0500

    changes in starRating component

--- app/javascript/components/Search/Filters/StartRating.js
+++ app/javascript/components/Search/Filters/StartRating.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSearchContext } from "../SearchProvider";
 import { Card } from "tabler-react";
 import Typography from "@material-ui/core/Typography";
@@ -8,6 +8,8 @@ function StarRating() {
   const {
     startRating, setStarRating
   } = useSearchContext();
+
+  const [ratings, setRatings] = useState([5,4,3,2,1])
 
   const handleChange = (event) => {
     let rating = event.target.value;
@@ -25,16 +27,14 @@ function StarRating() {
         <Typography id="range-slider" gutterBottom>
           Star Rating
         </Typography>
-        <input type="checkbox" value={5} onClick={handleChange}/>
-        <Rating name="customized-5" className="pl-2" value={5} max={5} readOnly/> <br />
-        <input type="checkbox" value={4} onClick={handleChange}/>
-        <Rating name="customized-4" className="pl-2" value={4} max={4} readOnly/><br />
-        <input type="checkbox" value={3} onClick={handleChange}/>
-        <Rating name="customized-3" className="pl-2" value={3} max={3} readOnly/><br />
-        <input type="checkbox" value={2} onClick={handleChange}/>
-        <Rating name="customized-2" className="pl-2" value={2} max={2} readOnly/><br />
-        <input type="checkbox" value={1} onClick={handleChange}/>
-        <Rating name="customized-1" className="pl-2" value={1} max={1} readOnly/>
+        {ratings.map((single_rating) => {
+          return (
+            <>
+              <input type="checkbox" value={single_rating} onClick={handleChange}/>
+              <Rating className="pl-2" value={single_rating} max={single_rating} readOnly/> <br />
+            </>
+          )
+        })}
       </Card.Body>
     </Card>
   );
