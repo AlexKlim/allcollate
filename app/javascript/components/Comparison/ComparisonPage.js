8129ab7aae47506b384974683a3581e80e1553aa
Anton <antongavrilov88@gmail.com>
2021-05-20 21:28:26 +0200

    add comparison page header. Add skeleton of comparison table

--- app/javascript/components/Comparison/ComparisonPage.js
+++ app/javascript/components/Comparison/ComparisonPage.js
@@ -2,6 +2,7 @@ import React from 'react';
 import SuggestionForm from '../AutoSuggestionSearch/SuggestionForm'
 import ComparisonResults from './ComparisonResults';
 import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid'
 
 import { useComparisonContext } from './ComparisonProvider';
 
@@ -20,26 +21,33 @@ function SearchPage() {
 
   return (
     <>
-      <div className='row'>
-        <div className='comparison__header'>
-          <div className='comparison__header_title'>
+      <Grid container spacing={2}>
+        <Grid item xs={4}>
+          <Grid className='comparison__title'>
             <p>
               Compare Hotels
-        </p>
-          </div>
-          <div className='comparison__header_inputblock'>
+            </p>
+          </Grid>
+        </Grid>
+        <Grid item xs={7}>
+          <Grid
+            container
+            direction="row"
+            justify="flex-start"
+            alignItems="center">
             <SuggestionForm
               onSuggestionSelected={onSuggestionSelected}
               onSubmit={onSubmit}
             />
-          </div>
-        </div>
-        <div className='row'>
-          <div className='col-md-12'>
-            <ComparisonResults />
-          </div>
-        </div>
-      </div>
+            <Grid item>
+              <a href='#'>Clear all</a>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+      <Grid>
+        <ComparisonResults />
+      </Grid>
     </>
   );
 }
--- app/javascript/components/Comparison/ComparisonResults.js
+++ app/javascript/components/Comparison/ComparisonResults.js
@@ -1,19 +1,84 @@
 import React from 'react';
 
 import { useComparisonContext } from './ComparisonProvider';
+import Typography from '@material-ui/core/Typography';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 
 function ComparisonResults() {
   const { hotels } = useComparisonContext();
 
+  const comparisonOptions = [
+    'Start Rating',
+    'Year Opened',
+    'Year Renovated',
+    'Checkin',
+    'Checkout',
+    'Number Rooms',
+    'Number Floors'
+  ]
+
   return (
     <>
-      {hotels.map((hotel, index) => {
-        return (
-          <div>{hotel.name}</div>
-        );
-      })}
+      <TableContainer>
+        <Table aria-label="customized table">
+          <TableHead>
+            <TableRow>
+              <TableCell />
+              {hotels.map((hotel, i) => {
+                return (
+                  <TableCell>{hotel.name}</TableCell>
+                )
+              })
+              }
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {
+              comparisonOptions.map(option => {
+                return(
+                <TableRow>
+                  <TableCell>
+                    {option}
+                  </TableCell>
+                  {hotels.map((hotel, i) => {
+                    return (
+                      <TableCell>{hotel.name}</TableCell>
+                    )
+                  })
+                  }
+                </TableRow>
+                )
+              })
+            }
+          </TableBody>
+        </Table>
+      </TableContainer>
     </>
   );
 }
 
 export default ComparisonResults;
+
+
+
+  //         <StyledTableCell align="right">Calories</StyledTableCell>
+  //         <StyledTableCell align="right">Fat&nbsp;(g)</StyledTableCell>
+  //         <StyledTableCell align="right">Carbs&nbsp;(g)</StyledTableCell>
+  //         <StyledTableCell align="right">Protein&nbsp;(g)</StyledTableCell>
+  //       {rows.map((row) => (
+  //         <StyledTableRow key={row.name}>
+  //           <StyledTableCell component="th" scope="row">
+  //             {row.name}
+  //           </StyledTableCell>
+  //           <StyledTableCell align="right">{row.calories}</StyledTableCell>
+  //           <StyledTableCell align="right">{row.fat}</StyledTableCell>
+  //           <StyledTableCell align="right">{row.carbs}</StyledTableCell>
+  //           <StyledTableCell align="right">{row.protein}</StyledTableCell>
+  //         </StyledTableRow>
+  //       ))}
