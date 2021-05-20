92ab54c5f53e7116a485166337c5e9df457fdd8b
Anton <antongavrilov88@gmail.com>
2021-05-20 21:29:21 +0200

    remove useless comments

--- app/javascript/components/Comparison/ComparisonResults.js
+++ app/javascript/components/Comparison/ComparisonResults.js
@@ -64,21 +64,3 @@ function ComparisonResults() {
 }
 
 export default ComparisonResults;
-
-
-
-  //         <StyledTableCell align="right">Calories</StyledTableCell>
-  //         <StyledTableCell align="right">Fat&nbsp;(g)</StyledTableCell>
-  //         <StyledTableCell align="right">Carbs&nbsp;(g)</StyledTableCell>
-  //         <StyledTableCell align="right">Protein&nbsp;(g)</StyledTableCell>
-  //       {rows.map((row) => (
-  //         <StyledTableRow key={row.name}>
-  //           <StyledTableCell component="th" scope="row">
-  //             {row.name}
-  //           </StyledTableCell>
-  //           <StyledTableCell align="right">{row.calories}</StyledTableCell>
-  //           <StyledTableCell align="right">{row.fat}</StyledTableCell>
-  //           <StyledTableCell align="right">{row.carbs}</StyledTableCell>
-  //           <StyledTableCell align="right">{row.protein}</StyledTableCell>
-  //         </StyledTableRow>
-  //       ))}
