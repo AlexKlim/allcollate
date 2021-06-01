93446670ca50c8142b8be8e6aae262774089af77
Anton <antongavrilov88@gmail.com>
2021-06-01 19:44:00 +0200

    remove unique-hotels method

--- app/javascript/components/Comparison/helpers.js
+++ app/javascript/components/Comparison/helpers.js
@@ -1,18 +1,3 @@
-export function uniqueHotels(hotels, cb) {
-    let uniqueHotelsNames = []
-    let uniqueHotels = []
-    for (let hotel of hotels) {
-        if (uniqueHotelsNames.indexOf(hotel.name) === -1) {
-            uniqueHotelsNames.push(hotel.name)
-            uniqueHotels.push(hotel)
-        }
-         else {
-            cb()
-        }
-    }
-    return uniqueHotels
-}
-
 export function hotelNameTrancate(hotelName) {
     const maxLength = 21
     return hotelName.length <= maxLength ? hotelName : hotelName.slice(0, maxLength) + '...'
