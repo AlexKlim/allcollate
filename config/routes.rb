5cf358ce9af44927a843828e9b03189d83e0068b
Alex Klimenkov <alex.klimenkovv@gmail.com>
2021-06-13 12:50:08 +0200

    add login window

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root to: 'home#index'
+
+  get '/auth/auth0/callback' => 'auth0#callback'
+  get '/auth/failure' => 'auth0#failure'
+  get '/auth/logout' => 'auth0#logout'
+
   get 'ping', to: 'home#ping'
 
   post 'subscriptions', to: 'subscriptions#create'
