f25f4e5f6407f7dbdd789eaa8ed0b6f1d8491d5b
Anton <antongavrilov88@gmail.com>
2021-07-04 21:57:52 +0200

    add hotel cards

--- app/javascript/components/LandingPage/LandingPage.js
+++ app/javascript/components/LandingPage/LandingPage.js
@@ -1,22 +1,41 @@
-import React from 'react'
+import React from 'react';
 import { Box, Typography } from '@material-ui/core';
+import SuggestionForm from '../AutoSuggestionSearch/SuggestionForm';
+import HotelCard from './HotelCard'
 
 const LandingPage = () => {
+
+    const onSuggestionSelected = (that, suggestion, value) => {
+        // setSlug(suggestion.slug);
+        // that.setState({ query: '' });
+        console.log(that, suggestion, value)
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+    }
     return (
         <>
-            <Box component='header' className='text-center  bg-light' style={{height: '578px;'}}>
+            <Box component='header' className='text-center  bg-light' style={{ height: '578px' }}>
                 <Box className='overlay'>
                     <Box className='container'>
                         <Box className='row'>
-                            <Box className='col-xl-9 mx-auto' style={{marginTop: '234px;'}}>
+                            <Box className='col-xl-9 mx-auto' style={{ marginTop: '234px' }}>
                                 <Typography variant="h1" component="h2" className='mb-5'>
                                     Allcollate
                                 </Typography>
-                                    <br />
-                                    Find and track any hotels
-                                    <br />
+                                <br />
+                                Find and track any hotels
+                                <br />
+                                <Typography>
                                     Suggestion form
-                            </Box>                        
+                                </Typography>
+                                {/* <SuggestionForm
+                                    onSuggestionSelected={onSuggestionSelected}
+                                    onSubmit={onSubmit}
+                                    withoutSearchButton={true}
+                                /> */}
+                            </Box>
                         </Box>
                     </Box>
                 </Box>
@@ -26,11 +45,26 @@ const LandingPage = () => {
                     <Box className='row'>
                         <Box className='col-lg-4'>
                             <Box className='features-icons-item mx-auto mb-5.mb-lg-0 mb-lg-3'>
-                                <Box className='features-icons-icon d-flex'>
-                                    <Box className='icon-directions m-auto text-primary'>
-                                        Test
-                                    </Box>
-                                </Box>
+                                <div className='features-icons-icon d-flex' style={{
+                                    display: 'flex',
+                                    flexWrap: 'wrap',
+                                    justifyContent: 'space-around',
+                                    flexDirection: 'row',
+                                    width: 'max-content',
+                                    marginLeft: '35%'
+                                    }}>
+                                    <div style={{ display: 'flex', flexWrap: 'wrap'}}>
+                                        <div style={{ width: '234px' }}>
+                                            <HotelCard />
+                                        </div>
+                                        <div style={{ width: '234px' }}>
+                                            <HotelCard />
+                                        </div>
+                                        <div style={{ width: '234px' }}>
+                                            <HotelCard />
+                                        </div>
+                                    </div>
+                                </div>
                             </Box>
                         </Box>
                     </Box>
