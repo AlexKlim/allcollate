3be68fbd9350a49b5622a4f71d50e8a388b1b959
Anton <antongavrilov88@gmail.com>
2021-07-11 14:19:24 +0200

    set opacity of header pictures to 0.25

---
+++ app/javascript/components/LandingPage/HeaderGraphic.js
@@ -0,0 +1,222 @@
+import React from 'react'
+import { Box } from '@material-ui/core';
+
+const HeaderGraphic = () => {
+    return (
+        <>
+            <Box style={{
+                position: 'absolute',
+                width: '130px',
+                height: '5px',
+                left: '464px',
+                top: '104px',
+                background: '#A49D9D',
+                borderRadius: '5px',
+                opacity: '0.25'
+            }} />
+            <Box style={{
+                position: 'absolute',
+                width: '70px',
+                height: '5px',
+                left: '464px',
+                top: '119px',
+                background: '#A49D9D',
+                borderRadius: '5px',
+                opacity: '0.25'
+            }} />
+            <Box style={{
+                position: 'absolute',
+                width: '110px',
+                height: '5px',
+                left: '464px',
+                top: '134px',
+                background: '#A49D9D',
+                borderRadius: '5px',
+                opacity: '0.25'
+            }} />
+            <Box style={{
+                position: 'absolute',
+                width: '50px',
+                height: '5px',
+                left: '464px',
+                top: '149px',
+
+                background: '#A49D9D',
+                borderRadius: '5px',
+                opacity: '0.25'
+            }} />
+            <Box style={{
+                position: 'absolute',
+                width: '550px',
+                height: '0px',
+                left: '335px',
+                top: '0px',
+
+                border: '1px solid #C3BBBB',
+                transform: 'rotate(90deg)',
+                opacity: '0.25'
+            }} />
+            <Box style={{
+                position: 'absolute',
+                width: '550px',
+                height: '0px',
+                left: '175px',
+                top: '200px',
+
+                border: '1px solid #C3BBBB',
+                transform: 'rotate(90deg)',
+                opacity: '0.25'
+            }} />
+            <Box style={{
+                position: 'absolute',
+                width: '800px',
+                height: '0px',
+                left: '0px',
+                top: '50px',
+
+                border: '1px solid #C4BBBB',
+                opacity: '0.25'
+            }} />
+            <Box style={{
+                position: 'absolute',
+                width: '130px',
+                height: '5px',
+                left: '630px',
+                top: '64px',
+
+                background: '#A49D9D',
+                borderRadius: '5px',
+                opacity: '0.25'
+            }} />
+            <Box style={{
+                position: 'absolute',
+                width: '70px',
+                height: '5px',
+                left: '630px',
+                top: '79px',
+
+                background: '#A49D9D',
+                borderRadius: '5px',
+                opacity: '0.25'
+            }} />
+            <Box style={{
+                position: 'absolute',
+                width: '110px',
+                height: '5px',
+                left: '630px',
+                top: '94px',
+                background: '#A49D9D',
+                borderRadius: '5px',
+                opacity: '0.25'
+            }} />
+            <Box style={{
+                position: 'absolute',
+                width: '50px',
+                height: '5px',
+                left: '630px',
+                top: '109px',
+                background: '#A49D9D',
+                borderRadius: '5px',
+                opacity: '0.25'
+            }} />
+            <Box style={{
+                position: 'absolute',
+                width: '130px',
+                height: '5px',
+                left: '630px',
+                top: '164px',
+
+                background: '#A49D9D',
+                borderRadius: '5px',
+                opacity: '0.25'
+            }} />
+            <Box style={{
+                position: 'absolute',
+                width: '70px',
+                height: '5px',
+                left: '630px',
+                top: '179px',
+
+                background: '#A49D9D',
+                borderRadius: '5px',
+                opacity: '0.25'
+            }} />
+            <Box style={{
+                position: 'absolute',
+                width: '110px',
+                height: '5px',
+                left: '630px',
+                top: '194px',
+                background: '#A49D9D',
+                borderRadius: '5px',
+                opacity: '0.25'
+            }} />
+            <Box style={{
+                position: 'absolute',
+                width: '50px',
+                height: '5px',
+                left: '630px',
+                top: '209px',
+                background: '#A49D9D',
+                borderRadius: '5px',
+                opacity: '0.25'
+            }} />
+
+            <Box style={{
+                position: 'absolute',
+                width: '130px',
+                height: '5px',
+                left: '300px',
+                top: '337px',
+
+                background: '#A49D9D',
+                borderRadius: '5px',
+                opacity: '0.25'
+            }} />
+            <Box style={{
+                position: 'absolute',
+                width: '70px',
+                height: '5px',
+                left: '300px',
+                top: '352px',
+
+                background: '#A49D9D',
+                borderRadius: '5px',
+                opacity: '0.25'
+            }} />
+            <Box style={{
+                position: 'absolute',
+                width: '110px',
+                height: '5px',
+                left: '300px',
+                top: '367px',
+                background: '#A49D9D',
+                borderRadius: '5px',
+                opacity: '0.25'
+            }} />
+            <Box style={{
+                position: 'absolute',
+                width: '50px',
+                height: '5px',
+                left: '300px',
+                top: '382px',
+                background: '#A49D9D',
+                borderRadius: '5px',
+                opacity: '0.25'
+            }} />
+            <Box style={{
+                position: 'absolute',
+                width: '550px',
+                height: '0px',
+                left: '15px',
+                top: '200px',
+
+                border: '1px solid #C3BBBB',
+                transform: 'rotate(90deg)',
+                opacity: '0.25'
+            }} />
+        </>
+    )
+}
+
+export default HeaderGraphic
