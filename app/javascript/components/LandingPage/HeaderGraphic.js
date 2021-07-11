d7af3fd7f749cb6bc7ce154eb5aaee3870432f55
Anton <antongavrilov88@gmail.com>
2021-07-11 16:46:06 +0200

    add two sectors to to the round diagram in the header pictures

--- app/javascript/components/LandingPage/HeaderGraphic.js
+++ app/javascript/components/LandingPage/HeaderGraphic.js
@@ -282,12 +282,35 @@ const HeaderGraphic = () => {
                 position: 'absolute',
                 left: '530px',
                 top: '230px',
-                border: '1px solid #F5F7FB',
+                // border: '1px solid #F5F7FB',
                 opacity: '0.25'
             }}>
                 <svg width="98" height="119" viewBox="0 0 98 119" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M39.2901 0.94864C48.6709 0.94671 57.9125 3.24021 66.2244 7.63291C74.5362 12.0256 81.6708 18.3868 87.0178 26.1722C92.3648 33.9576 95.7651 42.9355 96.9276 52.3378C98.0902 61.7401 96.9805 71.2869 93.6934 80.1615C90.4064 89.0361 85.0399 96.9743 78.053 103.297C71.066 109.62 62.6668 114.139 53.5737 116.467C44.4805 118.796 34.9643 118.865 25.8391 116.668C16.7139 114.471 8.25125 110.075 1.17529 103.854L39.302 59.6074L39.2901 0.94864Z" fill="#7CB5EC" stroke="#F5F7FB" stroke-linejoin="round" />
                 </svg>
+            </Box>
+            <Box style={{
+                position: 'absolute',
+                left: '508px',
+                top: '285px',
+                // border: '1px solid #F5F7FB',
+                opacity: '0.25'
+            }}>
+                <svg width="60" height="46" viewBox="0 0 60 46" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M20.4114 45.1454C9.44542 35.4852 2.46919 22.0189 0.863281 7.41145L58.5818 0.937439L20.4114 45.1454Z" fill="#434348" stroke="#F5F7FB" stroke-linejoin="round" />
+                </svg>
+            </Box>
+            <Box style={{
+                position: 'absolute',
+                left: '508px',
+                top: '253px',
+                // border: '1px solid #F5F7FB',
+                opacity: '0.25'
+            }}>
+                <svg width="60" height="40" viewBox="0 0 60 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M0.856792 39.3533C-0.596953 26.0083 2.52538 12.5634 9.7025 1.2634L58.5817 32.9376L0.856792 39.3533Z" fill="#90ED7D" stroke="#F5F7FB" stroke-linejoin="round" />
+                </svg>
+
             </Box>
         </>
     )
