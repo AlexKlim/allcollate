77b518f4f670de98243a78a19019267555742e47
Anton <antongavrilov88@gmail.com>
2021-07-11 16:31:42 +0200

    add round diagram to the header pictures

--- app/javascript/components/LandingPage/HeaderGraphic.js
+++ app/javascript/components/LandingPage/HeaderGraphic.js
@@ -50,7 +50,7 @@ const HeaderGraphic = () => {
                 width: '550px',
                 height: '0px',
                 left: '335px',
-                top: '0px',
+                top: '-50px',
 
                 border: '1px solid #C3BBBB',
                 transform: 'rotate(90deg)',
@@ -277,6 +277,17 @@ const HeaderGraphic = () => {
                 opacity: '0.25'
             }}>
                 Compare
+            </Box>
+            <Box style={{
+                position: 'absolute',
+                left: '530px',
+                top: '230px',
+                border: '1px solid #F5F7FB',
+                opacity: '0.25'
+            }}>
+                <svg width="98" height="119" viewBox="0 0 98 119" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M39.2901 0.94864C48.6709 0.94671 57.9125 3.24021 66.2244 7.63291C74.5362 12.0256 81.6708 18.3868 87.0178 26.1722C92.3648 33.9576 95.7651 42.9355 96.9276 52.3378C98.0902 61.7401 96.9805 71.2869 93.6934 80.1615C90.4064 89.0361 85.0399 96.9743 78.053 103.297C71.066 109.62 62.6668 114.139 53.5737 116.467C44.4805 118.796 34.9643 118.865 25.8391 116.668C16.7139 114.471 8.25125 110.075 1.17529 103.854L39.302 59.6074L39.2901 0.94864Z" fill="#7CB5EC" stroke="#F5F7FB" stroke-linejoin="round" />
+                </svg>
             </Box>
         </>
     )
