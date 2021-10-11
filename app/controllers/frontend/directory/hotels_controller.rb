e0127fbddc496882b41bc5cfb94cacccf41661ec
Alex Klimenkov <alex.klimenkovv@gmail.com>
2021-10-12 00:07:33 +0300

    add 301 redirect for old direct urls

--- app/controllers/frontend/directory/hotels_controller.rb
+++ app/controllers/frontend/directory/hotels_controller.rb
@@ -4,6 +4,12 @@ class Frontend::Directory::HotelsController < Frontend::BaseController
   PER_PAGE = 75
 
   def show
+    # Moved permanently for the legacy URLs
+    if params[:page]
+      path = "#{params[:prefix]}-#{params[:page]}"
+      redirect_to hotel_directory_path(path), status: :moved_permanently and return
+    end
+
     @prefix, @page = params[:prefix].split('-')
     @hotels = if @prefix == 'more'
                 Hotel.active.ransack(name_not_start_all: ('a'..'z'))
