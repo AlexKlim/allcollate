bd4cf31dfa8465294b64b65fde4bf3d48b383195
Alex Klimenkov <AlexKlim@users.noreply.github.com>
2024-08-01 12:06:20 +0200

    Update base_controller.rb

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -3,6 +3,7 @@ class Api::BaseController < ApplicationController
 
   # Method to log login attempts
   def log_login_attempt(user_id:, attempted_at:, successful:, ip_address:)
+    asd = 123
     LoginAttempt.log_attempt(
       user_id: user_id,
       attempted_at: attempted_at,
