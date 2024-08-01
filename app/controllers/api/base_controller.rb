37cdb8f2a0035c8b459b2300ae5ab320cfe6c4f6
Alex Klimenkkov <alex.klimenkovv@gmail.com>
2024-08-01 16:55:01 +0200

    test

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -1,3 +1,14 @@
 class Api::BaseController < ApplicationController
   layout nil
+
+  # Method to log login attempts
+  def log_login_attempt(user_id:, attempted_at:, successful:, ip_address:)
+    asd = 123
+    LoginAttempt.log_attempt(
+      user_id: user_id,
+      attempted_at: attempted_at,
+      successful: successful,
+      ip_address: ip_address
+    )
+
 end
