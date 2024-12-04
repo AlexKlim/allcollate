3a69eb57a649ed2d620bc7c2080fcc1607f19fcc
Neila (Jitera) <developers@jitera.com>
2024-12-04 14:31:29 +0000

    fix: 1 issues from deployment report

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 require 'bcrypt'
 
 class User < ApplicationRecord
+  attribute :email, :string
+  attribute :password, :string
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
